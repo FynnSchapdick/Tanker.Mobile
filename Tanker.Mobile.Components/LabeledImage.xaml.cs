068eda972cc70ebdb1eeb168d54bc15972df3050
FynnSchapdick <fschapdick@gmail.com>
2021-11-02 08:05:03 +0100

    wip

---
+++ Tanker.Mobile.Components/LabeledImage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tanker.Mobile.Components
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LabeledImage : Grid
+    {
+        public static BindableProperty ImageSourceProperty =
+            BindableProperty.Create(nameof(ImageSource), typeof(ImageSource), typeof(LabeledImage), null, BindingMode.TwoWay);
+
+        public ImageSource ImageSource
+        {
+            get => (ImageSource) GetValue(ImageSourceProperty);
+            set => SetValue(ImageSourceProperty, value);
+        }
+        
+        public static BindableProperty TextProperty =
+            BindableProperty.Create(nameof(Text), typeof(string), typeof(LabeledImage), string.Empty, BindingMode.TwoWay);
+        
+        public string Text
+        {
+            get => (string) GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        public LabeledImage()
+        {
+            InitializeComponent();
+        }
+    }
+}
